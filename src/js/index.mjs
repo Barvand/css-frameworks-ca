d25437dd8cf27ee89a358facd6d62dd2e0f6f4f7
B <128028346+Barvand@users.noreply.github.com>
2024-04-23 19:41:43 +0200

    create/read/update/delete
    Created the designated folders and files. Not code added. Created an example to see if code functioned as intended.

--- src/js/index.mjs
+++ src/js/index.mjs
@@ -4,12 +4,21 @@
 
 import { setRegisterFormListener } from "./handlers/register.mjs";
 import { setLoginFormListener } from "./handlers/login.mjs";
+import { createPost } from "./api/posts/create.mjs"
+
+import * as posts from "./api/posts/index.mjs"
+
 
 const path = location.pathname; 
-
 
 if (path === "/profile/register/index.html") { 
     setRegisterFormListener() 
 } else if (path === "/profile/login/index.html") { 
     setLoginFormListener()
 }
+
+
+createPost({
+    title:" Example", 
+    body: "Also example"
+})
