165ff5ec4c49379982a51498d09c9ff7a39ad993
B <128028346+Barvand@users.noreply.github.com>
2024-05-06 13:03:29 +0200

    bug in search bar fix

--- src/js/filters/searchBar.mjs
+++ src/js/filters/searchBar.mjs
@@ -7,44 +7,43 @@ export async function searchBar() {
   const searchInput = document.getElementById("searchBar");
   const searchResultsContainer = document.querySelector("#feed-container");
 
- const allPosts = await getPosts();
+  const allPosts = await getPosts();
 
-function performSearch(query) {
-      searchResultsContainer.innerHTML = "";
+  function performSearch(query) {
+    searchResultsContainer.innerHTML = "";
 
-      // If query is an empty string
-      if (query === "") {
-        allPosts.forEach((post) => {
-          createPostsHTML(post, searchResultsContainer);
-        });
-        loadButton.style.display = "block"; // Show load more button
-        return;
-      }
-
-      // Filter posts based on the query
-      const filteredPosts = allPosts.filter((post) => {
-        return (
-          query === "" ||
-          post.title.toLowerCase().includes(query.toLowerCase()) ||
-          post.id.toString().toLowerCase().includes(query.toLowerCase())
-        );
-      });
-
-      console.log(filteredPosts)
-
-      const displayedPosts = filteredPosts;
-
-      // Render search results
-      displayedPosts.forEach((post) => {
+    // If query is an empty string
+    if (query === "") {
+      allPosts.forEach((post) => {
         createPostsHTML(post, searchResultsContainer);
       });
+            return;
+    }
 
-    // Event listener for input field value changes
-    searchInput.addEventListener("input", function () {
-      const query = searchInput.value.trim();
-      performSearch(query);
+    // Filter posts based on the query
+    const filteredPosts = allPosts.filter((post) => {
+      return (
+        post.title.toLowerCase().includes(query.toLowerCase()) ||
+        post.id.toString().toLowerCase().includes(query.toLowerCase())
+      );
     });
-}};
 
+    // Render search results
+    filteredPosts.forEach((post) => {
+      createPostsHTML(post, searchResultsContainer);
+    });
+  }
 
-searchBar(); // Pass the container id as an argument
+  // Initial call to performSearch to display all posts
+  performSearch("");
+
+  // Event listener for input field value changes
+  searchInput.addEventListener("input", function () {
+    const query = searchInput.value.trim();
+    performSearch(query);
+  });
+}
+
+// Call searchBar function to initialize the search functionality
+searchBar();
+
